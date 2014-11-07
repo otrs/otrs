1f90679df926e40ba6115361cdc68d1156b316a1
Retiebralanak <jan@steinwegs.de>
2014-11-07 16:51:05 +0100

    Update AgentTicketProcess.pm
    Die div-class Namen werden richtig mit den DynamicField-Namen beschriftet

--- Kernel/Modules/AgentTicketProcess.pm
+++ Kernel/Modules/AgentTicketProcess.pm
@@ -2244,7 +2244,7 @@ sub _RenderDynamicField {
     );
 
     my %Data = (
-        Name    => $DynamicFieldHTML->{Name},
+        Name    => $DynamicFieldConfig->{Name},
         Label   => $DynamicFieldHTML->{Label},
         Content => $DynamicFieldHTML->{Field},
     );
