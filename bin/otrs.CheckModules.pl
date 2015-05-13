d978a444c44e9ed99811ce370b5846d962d0f342
Elias Probst <mail@eliasprobst.eu>
2015-05-13 15:38:33 +0200

    Add support for Gentoo/emerge in bin/otrs.CheckModules.pl

--- bin/otrs.CheckModules.pl
+++ bin/otrs.CheckModules.pl
@@ -66,6 +66,10 @@ our %InstTypeToCMD = (
         CMD       => 'apt-get install -y %s',
         UseModule => 0,
     },
+    emerge => {
+        CMD       => 'emerge \'%s\'',
+        UseModule => 0,
+    },
     ppm => {
         CMD       => 'ppm install %s',
         UseModule => 0,
@@ -89,6 +93,11 @@ our %DistToInstType = (
     # apt-get
     debian => 'aptget',
     ubuntu => 'aptget',
+
+    # emerge
+    # for reasons unknown, some environments return "gentoo" (incl. the quotes)
+    '"gentoo"' => 'emerge',
+    gentoo     => 'emerge',
 
     # yum
     centos => 'yum',
@@ -152,6 +161,7 @@ my @NeededModules = (
         Comment   => 'Avoids web server restarts on mod_perl.',
         InstTypes => {
             aptget => 'libapache2-mod-perl2',
+            emerge => 'dev-perl/Apache-Reload',
             ppm    => 'mod_perl-2.0',
             zypper => 'apache2-mod_perl',
         },
@@ -161,6 +171,7 @@ my @NeededModules = (
         Required  => 1,
         Comment   => 'Required for compressed file generation (in perlcore).',
         InstTypes => {
+            emerge => 'perl-core/Archive-Tar',
             zypper => 'perl-Archive-Tar',
         },
     },
@@ -170,6 +181,7 @@ my @NeededModules = (
         Comment   => 'Required for compressed file generation.',
         InstTypes => {
             aptget => 'libarchive-zip-perl',
+            emerge => 'dev-perl/Archive-Zip',
             zypper => 'Archive-Zip',
             zypper => 'perl-Archive-Zip',
         },
@@ -180,6 +192,7 @@ my @NeededModules = (
         Comment   => 'For strong password hashing.',
         InstTypes => {
             aptget => 'libcrypt-eksblowfish-perl',
+            emerge => 'dev-perl/Crypt-Eksblowfish',
             ppm    => 'Crypt-Eksblowfish',
             zypper => 'perl-Crypt-Eksblowfish',
         },
@@ -190,6 +203,7 @@ my @NeededModules = (
         Comment   => 'Required for Generic Interface REST transport and SOAP SSL connections.',
         InstTypes => {
             aptget => 'libcrypt-ssleay-perl',
+            emerge => 'dev-perl/Crypt-SSLeay',
             ppm    => 'Crypt-SSLeay',
             zypper => 'perl-Crypt-SSLeay',
         },
@@ -199,6 +213,7 @@ my @NeededModules = (
         Required  => 1,
         InstTypes => {
             aptget => 'libtimedate-perl',
+            emerge => 'dev-perl/TimeDate',
             ppm    => 'TimeDate',
             zypper => 'perl-TimeDate',
         },
@@ -208,6 +223,7 @@ my @NeededModules = (
         Required  => 1,
         InstTypes => {
             aptget => 'libdbi-perl',
+            emerge => 'dev-perl/DBI',
             ppm    => 'DBI',
             zypper => 'perl-DBI'
         },
@@ -218,6 +234,7 @@ my @NeededModules = (
         Comment   => 'Required to connect to a MySQL database.',
         InstTypes => {
             aptget => 'libdbd-mysql-perl',
+            emerge => 'dev-perl/DBD-mysql',
             ppm    => 'DBD-mysql',
             zypper => 'perl-DBD-mysql'
         },
@@ -235,6 +252,7 @@ my @NeededModules = (
         Comment   => 'Required to connect to a MS-SQL database.',
         InstTypes => {
             aptget => 'libdbd-odbc-perl',
+            emerge => undef,
             ppm    => 'DBD-ODBC',
             yum    => undef,
             zypper => undef,
@@ -246,6 +264,7 @@ my @NeededModules = (
         Comment   => 'Required to connect to a Oracle database.',
         InstTypes => {
             aptget => undef,
+            emerge => undef,
             ppm    => 'DBD-Oracle',
             yum    => undef,
             zypper => undef,
@@ -257,6 +276,7 @@ my @NeededModules = (
         Comment   => 'Required to connect to a PostgreSQL database.',
         InstTypes => {
             aptget => 'libdbd-pg-perl',
+            emerge => 'dev-perl/DBD-Pg',
             ppm    => 'DBD-Pg',
             zypper => 'perl-DBD-Pg',
         },
@@ -268,6 +288,7 @@ my @NeededModules = (
         Comment   => 'Required to handle mails with several Chinese character sets.',
         InstTypes => {
             aptget => 'libencode-hanextra-perl',
+            emerge => 'dev-perl/Encode-HanExtra',
             zypper => 'perl-Encode-HanExtra',
         },
     },
@@ -277,6 +298,7 @@ my @NeededModules = (
         Comment   => 'Required for stats.',
         InstTypes => {
             aptget => 'libgd-gd2-perl',
+            emerge => 'dev-perl/GD',
             ppm    => 'GD',
             zypper => 'perl-GD',
         },
@@ -287,6 +309,7 @@ my @NeededModules = (
                 Comment   => 'Required for stats.',
                 InstTypes => {
                     aptget => 'libgd-text-perl',
+                    emerge => 'dev-perl/GDTextUtil',
                     ppm    => 'GDTextUtil',
                     zypper => 'perl-GDTextUtil',
                 },
@@ -297,6 +320,7 @@ my @NeededModules = (
                 Comment   => 'Required for stats.',
                 InstTypes => {
                     aptget => 'libgd-graph-perl',
+                    emerge => 'dev-perl/GDGraph',
                     ppm    => 'GDGraph',
                     zypper => 'perl-GDGraph',
                 },
@@ -309,6 +333,7 @@ my @NeededModules = (
         Comment   => 'Required for SSL connections to web and mail servers.',
         InstTypes => {
             aptget => 'libio-socket-ssl-perl',
+            emerge => 'dev-perl/IO-Socket-SSL',
             ppm    => 'IO-Socket-SSL',
             zypper => 'perl-IO-Socket-SSL',
         },
@@ -319,6 +344,7 @@ my @NeededModules = (
         Comment   => 'Recommended for faster AJAX/JavaScript handling.',
         InstTypes => {
             aptget => 'libjson-xs-perl',
+            emerge => 'dev-perl/JSON-XS',
             ppm    => 'JSON-XS',
             zypper => 'perl-JSON-XS',
         },
@@ -330,6 +356,7 @@ my @NeededModules = (
             "Do a 'force install Scalar::Util' via cpan shell to fix this problem. Please make sure to have an c compiler and make installed before.",
         InstTypes => {
             aptget => 'libscalar-list-utils-perl',
+            emerge => 'perl-core/Scalar-List-Utils',
             ppm    => 'List-UtilsBy-XS',
             zypper => 'perl-Scalar-List-Utils',
         },
@@ -339,6 +366,7 @@ my @NeededModules = (
         Required  => 1,
         InstTypes => {
             aptget => 'libwww-perl',
+            emerge => 'dev-perl/libwww-perl',
             ppm    => 'libwww-perl',
             zypper => 'perl-libwww-perl',
         },
@@ -350,6 +378,7 @@ my @NeededModules = (
         Required  => 0,
         InstTypes => {
             aptget => 'libmail-imapclient-perl',
+            emerge => 'dev-perl/Mail-IMAPClient',
             ppm    => 'Mail-IMAPClient',
             zypper => 'perl-Mail-IMAPClient',
         },
@@ -360,6 +389,7 @@ my @NeededModules = (
                 Comment   => 'Required for IMAP TLS connections.',
                 InstTypes => {
                     aptget => 'libio-socket-ssl-perl',
+                    emerge => 'dev-perl/IO-Socket-SSL',
                     ppm    => 'IO-Socket-SSL',
                     zypper => 'perl-IO-Socket-SSL',
                 },
@@ -372,6 +402,7 @@ my @NeededModules = (
         Comment   => 'Improves Performance on Apache webservers dramatically.',
         InstTypes => {
             aptget => 'libapache2-mod-perl2',
+            emerge => 'www-apache/mod_perl',
             zypper => 'apache2-mod_perl',
         },
     },
@@ -387,6 +418,7 @@ my @NeededModules = (
         ],
         InstTypes => {
             aptget => 'libnet-dns-perl',
+            emerge => 'dev-perl/Net-DNS',
             ppm    => 'Net-DNS',
             zypper => 'perl-Net-DNS',
         },
@@ -397,6 +429,7 @@ my @NeededModules = (
         Comment   => 'Required for directory authentication.',
         InstTypes => {
             aptget => 'libnet-ldap-perl',
+            emerge => 'dev-perl/perl-ldap',
             ppm    => 'Net-LDAP',
             zypper => 'perl-ldap',
         },
@@ -430,6 +463,7 @@ my @NeededModules = (
         ],
         InstTypes => {
             aptget => 'libpdf-api2-perl',
+            emerge => 'dev-perl/PDF-API2',
             ppm    => 'PDF-API2',
             zypper => 'perl-PDF-API2',
         },
@@ -440,6 +474,7 @@ my @NeededModules = (
         Comment   => 'Template::Toolkit, the rendering engine of OTRS.',
         InstTypes => {
             aptget => 'libtemplate-perl',
+            emerge => 'dev-perl/Template-Toolkit',
             ppm    => 'Template-Toolkit',
             zypper => 'perl-Template-Toolkit',
         },
@@ -450,6 +485,7 @@ my @NeededModules = (
         Comment   => 'The fast data stash for Template::Toolkit.',
         InstTypes => {
             aptget => 'libtemplate-perl',
+            emerge => 'dev-perl/Template-Toolkit',
             ppm    => 'Template-Toolkit',
             zypper => 'perl-Template-Toolkit',
         },
@@ -460,6 +496,7 @@ my @NeededModules = (
         Comment   => 'Recommended for faster CSV handling.',
         InstTypes => {
             aptget => 'libtext-csv-xs-perl',
+            emerge => 'dev-perl/Text-CSV_XS',
             ppm    => 'Text-CSV_XS',
             zypper => 'perl-Text-CSV_XS',
         },
@@ -470,6 +507,7 @@ my @NeededModules = (
         Comment   => 'Required for high resolution timestamps.',
         InstTypes => {
             aptget => 'perl',
+            emerge => 'perl-core/Time-HiRes',
             ppm    => 'Time-HiRes',
             zypper => 'perl-Time-HiRes',
         },
@@ -486,6 +524,7 @@ my @NeededModules = (
         Comment   => 'Recommended for faster xml handling.',
         InstTypes => {
             aptget => 'libxml-parser-perl',
+            emerge => 'dev-perl/XML-Parser',
             ppm    => 'XML-Parser',
             zypper => 'perl-XML-Parser',
         },
@@ -496,6 +535,7 @@ my @NeededModules = (
         Comment   => 'Very important',
         InstTypes => {
             aptget => 'libyaml-libyaml-perl',
+            emerge => 'dev-perl/YAML-LibYAML',
             ppm    => 'YAML-XS',
             zypper => 'perl-YAML-LibYAML',
         },
