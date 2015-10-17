a556eee282611c4359103b5b3ad60435c888e075
Renee <reb@perl-services.de>
2015-10-17 12:11:47 +0200

    Set exact hour and minute for pending time iff configured - AgentTicketProcess

--- Kernel/Modules/AgentTicketProcess.pm
+++ Kernel/Modules/AgentTicketProcess.pm
@@ -2203,8 +2203,21 @@ sub _RenderPendingTime {
             %{ $Param{Ticket} },
         );
     }
+    
+    my %PendingOpts;
+    my $Hour = $ConfigObject->Get('Ticket::Frontend::PendingTimeHour');
+    if ( defined $Hour ) {
+        $PendingOpts{Hour} = $Hour;
+    }
 
-    $Data{Content} = $LayoutObject->BuildDateSelection(
+    my $Minute = $ConfigObject->Get('Ticket::Frontend::PendingTimeMinute');
+    if ( defined $Minute ) {
+        $PendingOpts{Minute} = $Minute;
+    }
+
+    # pending data string
+    $Param{PendingDateString} = $LayoutObject->BuildDateSelection(
+        %PendingOpts,
         Prefix => 'PendingTime',
         PendingTimeRequired =>
             (
