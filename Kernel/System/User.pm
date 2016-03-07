28f4a627f910351ceb150379341e60b18c60c5d2
Pawel Boguslawski <pawel.boguslawski@ib.pl>
2016-03-07 15:58:37 +0100

    Translatable out of office message
    Out of office strings in user name like
    
       User Name *** out of office till YYYY-D-M/2d ***
    
    should be translatable not hardcoded english string.
    
    Related: https://dev.ib.pl/ib/otrs/issues/24
    Author-Change-Id: IB#1004603

--- Kernel/System/User.pm
+++ Kernel/System/User.pm
@@ -16,6 +16,7 @@ use Digest::SHA;
 
 our @ObjectDependencies = (
     'Kernel::Config',
+    'Kernel::Language',
     'Kernel::System::Cache',
     'Kernel::System::CheckItem',
     'Kernel::System::DB',
@@ -281,7 +282,8 @@ sub GetUserData {
             my $TillDate
                 = "$Preferences{OutOfOfficeEndYear}-$Preferences{OutOfOfficeEndMonth}-$Preferences{OutOfOfficeEndDay}";
             if ( $TimeStart < $Time && $TimeEnd > $Time ) {
-                $Preferences{OutOfOfficeMessage} = "*** out of office till $TillDate/$Till d ***";
+                my $LanguageObject = $Kernel::OM->Get('Kernel::Language');
+                $Preferences{OutOfOfficeMessage} = '*** ' . $LanguageObject->Translate('out of office till') . " $TillDate ($Till " . $LanguageObject->Translate('days') . ') ***';
                 $Data{UserLastname} .= ' ' . $Preferences{OutOfOfficeMessage};
             }
 
