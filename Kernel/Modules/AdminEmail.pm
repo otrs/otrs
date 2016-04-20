otrs/otrs

commit 74fe26ecdb3be0fffe5551cce8349e0a40806193

Fixed bug#11944 - Inline images possible although image functionality is disabled.

var/httpd/htdocs/js/Core.UI.RichTextEditor.js
@@ -148,7 +148,7 @@ Core.UI.RichTextEditor = (function (TargetNS) {
             toolbar: CheckFormID().length ? Core.Config.Get('RichText.Toolbar') : Core.Config.Get('RichText.ToolbarWithoutImage'),
             filebrowserBrowseUrl: '',
             filebrowserUploadUrl: UploadURL,
-            extraPlugins: Core.Config.Get('RichText.SpellChecker') ? 'aspell,splitquote' : 'splitquote',
+            extraPlugins: Core.Config.Get('RichText.SpellChecker') ? 'aspell,splitquote,preventimagepaste' : 'splitquote,preventimagepaste',
             entities: false,
             skin: 'bootstrapck'
         });

var/httpd/htdocs/js/thirdparty/ckeditor-4.5.6/plugins/preventimagepaste/plugin.js
@@ -1,0 +1,31 @@
+CKEDITOR.plugins.add('preventimagepaste', {
+    init: function(editor) {
+
+        function replaceImages(content) {
+            if (!content) {
+                return "";
+            }
+            return content.replace(/<img[^>]*src="data:image\/(bmp|dds|gif|jpg|jpeg|png|psd|pspimage|tga|thm|tif|tiff|yuv|ai|eps|ps|svg);base64,.*?"[^>]*>/gi, '');
+        }
+
+        // plugin reacts on pasting data and dropping data (drop also triggers paste)
+        // this prevents paste of images (as base64-encoded data URIs), if the image plugin of CKEditor is not loaded
+        // if the plugin is loaded all pasted/dropped images are handled with a correct upload
+        editor.on('paste', function (event) {
+            if (editor.readOnly) {
+                return;
+            }
+
+            // if image plugin is active, we don't want to prevent pasting and dropping
+            if (typeof editor.plugins.image2 !== 'undefined') {
+                return;
+            }
+
+            // stop original paste event to edit pasted data first
+            event.stop();
+            if (event && event.data && event.data.dataValue) {
+                editor.insertHtml(replaceImages(event.data.dataValue));
+            }
+        });
+    }
+});

commit 7005c01e15de64afd63d763ae4cb166e5ad0460f

Image pasting as data URI reenabled
This mod reenables pasting images as data URIs to signatures, templates
and AdminEmail messages. It also fixes problem with embedding inline
images using copy/paste on messages sent from AdminEmail (now such
images will be converted to attachments in e-mail which is more
compatible with poor mail readers).

This mod introduces also new SysConfig option
Frontend::RichText::PreventImagePaste (disabled by default) which
allowes one to disable pasting images as data URIs to rich text areas.

Fixes: 74fe26ecdb3be0fffe5551cce8349e0a40806193
Related: https://dev.ib.pl/ib/otrs/issues/41
Related: http://bugs.otrs.org/show_bug.cgi?id=11944
Author-Change-Id: IB#1051028

Kernel/Config/Files/Framework.xml
@@ -449,6 +449,17 @@
             </Option>
         </Setting>
     </ConfigItem>
+    <ConfigItem Name="Frontend::RichText::PreventImagePaste" Required="0" Valid="0">
+        <Description Translatable="1">Defines if image pasting into rich text area as data URI should be disabled (data URI allows to embed inline images on AdminEmail messages, signatures and templates using image copy/paste in modern web browsers). Note: signature and template content size must not exceed target column size in database (i.e. 64 kB in MySQL) - too big images will cause signature/template content to be truncated and corrupted.</Description>
+        <Group>Framework</Group>
+        <SubGroup>Core::Web</SubGroup>
+        <Setting>
+            <Option SelectedID="0">
+                <Item Key="0" Translatable="1">No</Item>
+                <Item Key="1" Translatable="1">Yes</Item>
+            </Option>
+        </Setting>
+    </ConfigItem>
     <ConfigItem Name="DisableIFrameOriginRestricted" Required="0" Valid="1">
         <Description Translatable="1">Disable HTTP header "X-Frame-Options: SAMEORIGIN" to allow OTRS to be included as an IFrame in other websites. Disabling this HTTP header can be a security issue! Only disable it, if you know what you are doing!</Description>
         <Group>Framework</Group>

Kernel/Modules/AdminEmail.pm
@@ -158,10 +158,22 @@ sub Run {
                 $Param{Body} =~ s/(\r\n|\n\r)/\n/g;
                 $Param{Body} =~ s/\r/\n/g;
 
+                # initialize parameter for attachments, so that the content pushed into that ref from
+                # EmbeddedImagesExtract will stay available
+                if ( !$Param{Attachment} ) {
+                    $Param{Attachment} = [];
+                }
+
                 # get content type
                 my $ContentType = 'text/plain';
                 if ( $LayoutObject->{BrowserRichText} ) {
                     $ContentType = 'text/html';
+
+                    # check for base64 images in body and process them
+                    $Kernel::OM->Get('Kernel::System::HTMLUtils')->EmbeddedImagesExtract(
+                        DocumentRef    => \$Param{Body},
+                        AttachmentsRef => $Param{Attachment},
+                    );
 
                     # verify html document
                     $Param{Body} = $LayoutObject->RichTextDocumentComplete(
@@ -171,12 +183,13 @@ sub Run {
 
                 # send mail
                 my $Sent = $Kernel::OM->Get('Kernel::System::Email')->Send(
-                    From     => $Param{From},
-                    Bcc      => $Param{Bcc},
-                    Subject  => $Param{Subject},
-                    Charset  => $LayoutObject->{UserCharset},
-                    MimeType => $ContentType,
-                    Body     => $Param{Body},
+                    From       => $Param{From},
+                    Bcc        => $Param{Bcc},
+                    Subject    => $Param{Subject},
+                    Charset    => $LayoutObject->{UserCharset},
+                    MimeType   => $ContentType,
+                    Body       => $Param{Body},
+                    Attachment => $Param{Attachment},
                 );
                 if ( !$Sent ) {
                     return $LayoutObject->ErrorScreen();

Kernel/Output/HTML/Templates/Standard/CustomerRichTextEditor.tt
@@ -13,6 +13,7 @@
     Core.Config.Set('RichText.Height', '[% Config("Frontend::RichTextHeight") %]');
     Core.Config.Set('RichText.TextDir', '[% Env("TextDirection") %]');
     Core.Config.Set('RichText.SpellChecker', '[% Env("BrowserSpellCheckerInline") %]');
+    Core.Config.Set('RichText.PreventImagePaste', '[% Config("Frontend::RichText::PreventImagePaste") %]');
     Core.Config.Set('RichText.EditingAreaCSS', 'body { [% Config("Frontend::RichText::DefaultCSS") %] }');
     Core.Config.Set('RichText.Lang.SplitQuote', '[% Translate('Split Quote') | html %]');
     Core.Config.Set('RichText.Toolbar', [

Kernel/Output/HTML/Templates/Standard/RichTextEditor.tt
@@ -26,6 +26,7 @@
 
     Core.Config.Set('RichText.TextDir', '[% Env("TextDirection") %]');
     Core.Config.Set('RichText.SpellChecker', '[% Env("BrowserSpellCheckerInline") %]');
+    Core.Config.Set('RichText.PreventImagePaste', '[% Config("Frontend::RichText::PreventImagePaste") %]');
     Core.Config.Set('RichText.EditingAreaCSS', 'body.cke_editable { [% Config("Frontend::RichText::DefaultCSS") %] }');
     Core.Config.Set('RichText.Lang.SplitQuote', '[% Translate('Split Quote') | html %]');
     Core.Config.Set('RichText.Lang.RemoveQuote', '[% Translate('Remove Quote') | html %]');

var/httpd/htdocs/js/Core.UI.RichTextEditor.js
@@ -68,7 +68,8 @@ Core.UI.RichTextEditor = (function (TargetNS) {
         var EditorID = '',
             Editor,
             UserLanguage,
-            UploadURL = '';
+            UploadURL = '',
+            ExtraPlugins;
 
         if (isJQueryObject($EditorArea) && $EditorArea.hasClass('HasCKEInstance')) {
             return false;
@@ -127,6 +128,15 @@ Core.UI.RichTextEditor = (function (TargetNS) {
                     + '=' + Core.Config.Get('SessionID');
         }
 
+        // configure extra plugins
+        ExtraPlugins = 'splitquote';
+        if (Core.Config.Get('RichText.SpellChecker')) {
+            ExtraPlugins += ',aspell';
+        }
+        if (Core.Config.Get('RichText.PreventImagePaste') === '1') {
+            ExtraPlugins += ',preventimagepaste';
+        }
+
         /*eslint-disable camelcase */
         Editor = CKEDITOR.replace(EditorID,
         {
@@ -148,7 +158,7 @@ Core.UI.RichTextEditor = (function (TargetNS) {
             toolbar: CheckFormID().length ? Core.Config.Get('RichText.Toolbar') : Core.Config.Get('RichText.ToolbarWithoutImage'),
             filebrowserBrowseUrl: '',
             filebrowserUploadUrl: UploadURL,
-            extraPlugins: Core.Config.Get('RichText.SpellChecker') ? 'aspell,splitquote,preventimagepaste' : 'splitquote,preventimagepaste',
+            extraPlugins: ExtraPlugins,
             entities: false,
             skin: 'bootstrapck'
         });