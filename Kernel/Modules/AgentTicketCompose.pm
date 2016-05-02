otrs/otrs

commit a78dfffa46edf71f4e99a35c0c6121bd4e5f0b8d

Fixed wrong proposed default article type when replying/forwarding
When you reply to phone article (incoming or outgoing) AgentTicketCompose
form proposes email-internal as default article type if agent uses Polish
language in web frontend and email-external if agent uses English
language in web frontend.

When forwarding from article with internal type - email-external is
proposed as default in AgentTicketForward form.

With this fix OTRS proposes email-internal in AgentTicketForward and
AgentTicketCompose if sending from article with "internal"
type and email-external otherwise.

Related: https://dev.ib.pl/ib/otrs/issues/46
Author-Change-Id: IB#1014394

Kernel/Modules/AgentTicketCompose.pm
@@ -1685,7 +1685,20 @@ sub _Mask {
 
         my %Selected;
         if ( $Param{ArticleTypeID} ) {
-            $Selected{SelectedID} = $Param{ArticleTypeID};
+
+            # use email-internal article type if replying to article with internal type; otherwise use email-internal
+            my $ArticleTypeSelected = $TicketObject->ArticleTypeLookup( ArticleTypeID => $Param{ArticleTypeID} );
+            if ( $ArticleTypeSelected ) {
+                if ( $ArticleTypeSelected =~ m{internal} ) {
+                    $Selected{SelectedID} = $TicketObject->ArticleTypeLookup( ArticleType => 'email-internal' );
+                }
+                else {
+                    $Selected{SelectedID} = $TicketObject->ArticleTypeLookup( ArticleType => 'email-external' );
+                }
+            }
+            else {
+                $Selected{SelectedValue} = $Config->{DefaultArticleType};
+            }
         }
         else {
             $Selected{SelectedValue} = $Config->{DefaultArticleType};

Kernel/Modules/AgentTicketForward.pm
@@ -1370,8 +1370,21 @@ sub _Mask {
         }
 
         my %Selected;
-        if ( $Self->{GetParam}->{ArticleTypeID} ) {
-            $Selected{SelectedID} = $Self->{GetParam}->{ArticleTypeID};
+        if ( $Param{ArticleTypeID} ) {
+
+            # use email-internal article type if forwarding article with internal type; otherwise use email-internal
+            my $ArticleTypeSelected = $TicketObject->ArticleTypeLookup( ArticleTypeID => $Param{ArticleTypeID} );
+            if ( $ArticleTypeSelected ) {
+                if ( $ArticleTypeSelected =~ m{internal} ) {
+                    $Selected{SelectedID} = $TicketObject->ArticleTypeLookup( ArticleType => 'email-internal' );
+                }
+                else {
+                    $Selected{SelectedID} = $TicketObject->ArticleTypeLookup( ArticleType => 'email-external' );
+                }
+            }
+            else {
+                $Selected{SelectedValue} = $Config->{DefaultArticleType};
+            }
         }
         else {
             $Selected{SelectedValue} = $Config->{ArticleTypeDefault};

commit 40d5624924bb08f0e098f774c05fbccdc9533a5f

Internal article type preseved in AgentTicketCompose and AgentTicketForward
When composing messages in AgentTicketCompose and AgentTicketForward OTRS
uses parameters

    Ticket::Frontend::AgentTicketForward###ArticleTypeDefault
    Ticket::Frontend::AgentTicketCompose###DefaultArticleType

for proposing default new article type. This may lead to problems
with internal information disclosure in customer web panel (i.e. when
agent replies to or forwards internal message without changing default
"email-external" article type proposed by OTRS).

This mod introduces two SysConfig options

    Ticket::Frontend::AgentTicketCompose###DefaultArticleTypeInternal
    Ticket::Frontend::AgentTicketForward###ArticleTypeDefaultInternal

which allow to define default article type when composing/forwarding
e-mails from source article with type that contains "internal"
in its name. By default both new options are set to "email-internal"
which keeps default "internal" status of messages composed/forwarded
from other "internal" articles.

Fixes: a78dfffa46edf71f4e99a35c0c6121bd4e5f0b8d
Related: https://dev.ib.pl/ib/otrs/issues/49
Author-Change-Id: IB#1051354

Kernel/Config/Files/Ticket.xml
@@ -5394,6 +5394,14 @@
             <String Regex="">email-external</String>
         </Setting>
     </ConfigItem>
+    <ConfigItem Name="Ticket::Frontend::AgentTicketCompose###DefaultArticleTypeInternal" Required="0" Valid="1">
+        <Description Translatable="1">Specifies the default article type for the ticket compose screen in the agent interface if the article type cannot be automatically detected and source article type name contains "internal". Has higher priority than Ticket::Frontend::AgentTicketCompose###DefaultArticleType.</Description>
+        <Group>Ticket</Group>
+        <SubGroup>Frontend::Agent::Ticket::ViewCompose</SubGroup>
+        <Setting>
+            <String Regex="">email-internal</String>
+        </Setting>
+    </ConfigItem>
     <ConfigItem Name="Ticket::Frontend::ResponseFormat" Required="1" Valid="1">
         <Description Translatable="1">Defines the format of responses in the ticket compose screen of the agent interface ([% Data.OrigFrom | html %] is From 1:1, [% Data.OrigFromName | html %] is only realname of From).</Description>
         <Group>Ticket</Group>
@@ -5493,6 +5501,14 @@
         <SubGroup>Frontend::Agent::Ticket::ViewForward</SubGroup>
         <Setting>
             <String Regex="">email-external</String>
+        </Setting>
+    </ConfigItem>
+    <ConfigItem Name="Ticket::Frontend::AgentTicketForward###ArticleTypeDefaultInternal" Required="0" Valid="1">
+        <Description Translatable="1">Defines the default type of forwarded message in the ticket forward screen of the agent interface if the article type cannot be automatically detected and source article type name contains "internal". Has higher priority than Ticket::Frontend::AgentTicketForward###ArticleTypeDefault.</Description>
+        <Group>Ticket</Group>
+        <SubGroup>Frontend::Agent::Ticket::ViewForward</SubGroup>
+        <Setting>
+            <String Regex="">email-internal</String>
         </Setting>
     </ConfigItem>
     <ConfigItem Name="Ticket::Frontend::AgentTicketForward###ArticleTypes" Required="0" Valid="1">

Kernel/Modules/AgentTicketCompose.pm
@@ -1604,11 +1604,12 @@ sub Run {
             ReplyArticleID      => $GetParam{ArticleID},
             %Ticket,
             %Data,
-            InReplyTo        => $Data{MessageID},
-            References       => "$References",
-            TicketBackType   => $TicketBackType,
-            DynamicFieldHTML => \%DynamicFieldHTML,
-            ArticleTypeID    => $GetParam{ArticleTypeID},    # don't use the ArticleID from the previous article
+            InReplyTo           => $Data{MessageID},
+            References          => "$References",
+            TicketBackType      => $TicketBackType,
+            DynamicFieldHTML    => \%DynamicFieldHTML,
+            ArticleTypeID       => $GetParam{ArticleTypeID},
+            SourceArticleTypeID => $Data{ArticleTypeID},
         );
         $Output .= $LayoutObject->Footer(
             Type => 'Small',
@@ -1671,24 +1672,34 @@ sub _Mask {
     # get ticket object
     my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
 
-    #  get article type
-    my %ArticleTypeList;
-
-    if ( $Config->{ArticleTypes} ) {
+    if ( IsArrayRefWithData( $Config->{ArticleTypes} ) ) {
+        my %ArticleTypeList;
+        my %Selected;
 
         my @ArticleTypesPossible = @{ $Config->{ArticleTypes} };
-        for my $ArticleTypeID (@ArticleTypesPossible) {
-            my $ArticleType = $TicketObject->ArticleTypeLookup(
-                ArticleType => $ArticleTypeID,
+        for my $ArticleType (@ArticleTypesPossible) {
+            my $ArticleTypeID = $TicketObject->ArticleTypeLookup(
+                ArticleType => $ArticleType,
             );
-            $ArticleTypeList{$ArticleType} = $ArticleTypeID;
+            $ArticleTypeList{$ArticleTypeID} = $ArticleType;
+
+            # requested article type must be present in article types list
+            # or default will be used
+            if ( $Param{ArticleTypeID} && $ArticleTypeID == $Param{ArticleTypeID} ) {
+                $Selected{SelectedValue} = $ArticleType;
+            }
         }
 
-        my %Selected;
-        if ( $Param{ArticleTypeID} ) {
-            $Selected{SelectedID} = $Param{ArticleTypeID};
+        # default selected article type for internal source articles
+        if ( !$Selected{SelectedValue} && $Config->{DefaultArticleTypeInternal} && $Param{SourceArticleTypeID} ) {
+            my $SourceArticleType = $TicketObject->ArticleTypeLookup( ArticleTypeID => $Param{SourceArticleTypeID} );
+            if ( $SourceArticleType && $SourceArticleType =~ m{internal} ) {
+                $Selected{SelectedValue} = $Config->{DefaultArticleTypeInternal};
+            }
         }
-        else {
+
+        # default selected article type
+        if ( !$Selected{SelectedValue} && $Config->{DefaultArticleType} ) {
             $Selected{SelectedValue} = $Config->{DefaultArticleType};
         }
 

Kernel/Modules/AgentTicketForward.pm
@@ -562,9 +562,11 @@ sub Form {
         Attachments         => \@Attachments,
         %Data,
         %GetParam,
-        InReplyTo        => $Data{MessageID},
-        References       => $References,
-        DynamicFieldHTML => \%DynamicFieldHTML,
+        InReplyTo           => $Data{MessageID},
+        References          => $References,
+        DynamicFieldHTML    => \%DynamicFieldHTML,
+        ArticleTypeID       => $GetParam{ArticleTypeID},
+        SourceArticleTypeID => $Data{ArticleTypeID},
     );
     $Output .= $LayoutObject->Footer(
         Type => 'Small',
@@ -1351,29 +1353,37 @@ sub _Mask {
         %State,
     );
 
-    #  get article type
-    my %ArticleTypeList;
-
     # get ticket object
     my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
 
     if ( IsArrayRefWithData( $Config->{ArticleTypes} ) ) {
+        my %ArticleTypeList;
+        my %Selected;
 
         my @ArticleTypesPossible = @{ $Config->{ArticleTypes} };
         for my $ArticleType (@ArticleTypesPossible) {
-
             my $ArticleTypeID = $TicketObject->ArticleTypeLookup(
                 ArticleType => $ArticleType,
             );
-
             $ArticleTypeList{$ArticleTypeID} = $ArticleType;
+
+            # requested article type must be present in article types list
+            # or default will be used
+            if ( $Param{ArticleTypeID} && $ArticleTypeID == $Param{ArticleTypeID} ) {
+                $Selected{SelectedValue} = $ArticleType;
+            }
         }
 
-        my %Selected;
-        if ( $Self->{GetParam}->{ArticleTypeID} ) {
-            $Selected{SelectedID} = $Self->{GetParam}->{ArticleTypeID};
+        # default selected article type for internal source articles
+        if ( !$Selected{SelectedValue} && $Config->{ArticleTypeDefaultInternal} && $Param{SourceArticleTypeID} ) {
+            my $SourceArticleType = $TicketObject->ArticleTypeLookup( ArticleTypeID => $Param{SourceArticleTypeID} );
+            if ( $SourceArticleType && $SourceArticleType =~ m{internal} ) {
+                $Selected{SelectedValue} = $Config->{ArticleTypeDefaultInternal};
+            }
         }
-        else {
+
+        # default selected article type
+        if ( !$Selected{SelectedValue} && $Config->{ArticleTypeDefault} ) {
             $Selected{SelectedValue} = $Config->{ArticleTypeDefault};
         }
 