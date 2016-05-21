8d6a9812493f9383c274afe177ea2a6e82ed93a1
Thorsten Eckel <te@znuny.com>
2016-05-21 14:23:28 +0200

    Added 'eval' option and UnitTests.

--- Kernel/System/Console/Command/Dev/Tools/Shell.pm
+++ Kernel/System/Console/Command/Dev/Tools/Shell.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -22,23 +22,32 @@ sub Configure {
 
     $Self->Description('An interactive REPL shell for the OTRS context.');
 
+    $Self->AddOption(
+        Name        => 'eval',
+        Description => 'Perl code that should get evaluated and printed in the OTRS context.',
+        Required    => 0,
+        HasValue    => 1,
+        ValueRegex  => qr/.*/smx,
+    );
+
     return;
 }
 
 sub PreRun {
     my ( $Self, %Param ) = @_;
 
-    my @Dependencies = ('Devel::REPL', 'Data::Printer');
+    my @Dependencies = ( 'Devel::REPL', 'Data::Printer' );
 
     DEPENDENCY:
-    for my $Dependency ( @Dependencies ) {
+    for my $Dependency (@Dependencies) {
 
         next DEPENDENCY if $Kernel::OM->Get('Kernel::System::Main')->Require(
             $Dependency,
             Silent => 1,
         );
 
-        die "Required Perl module '$Dependency' not found. Please make sure the following dependencies are installed: ". join(' ', @Dependencies);
+        die "Required Perl module '$Dependency' not found. Please make sure the following dependencies are installed: "
+            . join( ' ', @Dependencies );
     }
 
     return 1;
@@ -49,19 +58,44 @@ sub Run {
 
     my $Repl = Devel::REPL->new();
 
-    for my $Plugin ( qw(History LexEnv MultiLine::PPI FancyPrompt OTRS) ) {
-        $Repl->load_plugin( $Plugin );
+    for my $Plugin (qw(History LexEnv MultiLine::PPI FancyPrompt OTRS)) {
+        $Repl->load_plugin($Plugin);
     }
 
     # fancy things are made with love <3
-    $Repl->fancy_prompt(sub {
-        my $self = shift;
-        sprintf 'OTRS: %03d%s> ',
-            $self->lines_read,
-            $self->can('line_depth') ? ':' . $self->line_depth : '';
-    });
+    $Repl->fancy_prompt(
+        sub {    ## no critic
+            my $self = shift;
+            sprintf 'OTRS: %03d%s> ',
+                $self->lines_read,    ## no critic
+                $self->can('line_depth') ? ':' . $self->line_depth : '';    ## no critic
+        }
+    );
 
-    $Repl->run();
+    my $Code = $Self->GetOption('eval');
+
+    if ($Code) {
+
+        # force the output to STDOUT
+        # so we can handle it properly
+        # in the UnitTest context
+        $Repl->{out_fh} = \*STDOUT;
+
+        # we need to push the code to the history
+        # to simulate the regular History functionality
+        # which is not called since we don't use
+        # the $Repl-read function to get the code
+        $Repl->push_history($Code);
+
+        # evaluate the code
+        my @Result = $Repl->eval($Code);
+
+        # and print it to STDOUT
+        $Repl->print(@Result) if !$Repl->exit_repl;    ## no critic
+    }
+    else {
+        $Repl->run();                                  ## no critic
+    }
 
     return $Self->ExitCodeOk();
 }
