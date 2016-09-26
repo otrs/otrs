5d336639c504ef4d8aa731e116d7dce4d2545826
Thorsten <thorsteneckel@posteo.de>
2016-09-26 08:29:01 +0200

    Bugfix 12324 TicketList: Preview shows all tickets
    See: http://bugs.otrs.org/show_bug.cgi?id=12324

--- Kernel/System/Stats/Dynamic/TicketList.pm
+++ Kernel/System/Stats/Dynamic/TicketList.pm
@@ -1007,7 +1007,9 @@ sub GetStatTable {
             UserID     => 1,
             Result     => 'ARRAY',
             Permission => 'ro',
-            Limit      => 10,
+            %{ $Param{Restrictions} },
+            %DynamicFieldRestrictions,
+            Limit => 10,
         );
     }
     else {
