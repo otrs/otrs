a6e012a3b250bc1a134b88966ecdf5398990274d
johannesbauer1 <j.bauer@goetz-fm.com>
2017-04-20 20:09:00 +0200

    Update zzzCallFunction.pm

--- Kernel/System/ProcessManagement/TransitionAction/zzzCallFunction.pm
+++ Kernel/System/ProcessManagement/TransitionAction/zzzCallFunction.pm
@@ -1,3 +1,10 @@
+# --
+# Copyright (C) 2001-2017 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
 package Kernel::System::ProcessManagement::TransitionAction::zzzCallFunction;
 
 use strict;
