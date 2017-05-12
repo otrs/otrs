f19472b7568819e3f4219076415be3949b04490f
gravasio <gravasio@kng.it>
2017-05-12 12:04:10 +0200

    Update AgentTicketForward.pm
    Checking BlockSystemAddressRecipient config

--- Kernel/Modules/AgentTicketForward.pm
+++ Kernel/Modules/AgentTicketForward.pm
@@ -858,7 +858,7 @@ sub SendEmail {
                 Address => $Email->address()
             );
 
-            if ($IsLocal) {
+            if ($IsLocal && $Config->{BlockSystemAddressRecipient} ) {
                 $Error{ $Line . 'IsLocalAddress' } = 'ServerError';
             }
         }
