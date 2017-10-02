otrs/otrs

commit 459ae466100b66d8f264b7d52d832103d2dcce7d

Attachment name validation added
OTRS does not validate attachment names passed in e-mail headers
and upload requests before using this values for filenaeme creation
in filesystem; this is risky (possible escaping quirks).

There is also problem with missing attachments with long names; if
one try to send e-mail from Windows with attachement

C:\tmp\0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę0123456ąćę001.txt

OTRS baked on XFS will throw an error

Message: Can't write '[...]0123456ąćę0123456ąćę012[...]001.txt.content_type': File name too long

and attachment won't be present in ticket. Similar problem when agent tries
to upload file as above from Windows workstation to OTRS baked on XFS.

This mod fixes both issues described above by filtering attachment names using
Kernel::System::Main::FilenameCleanUp function that was modified in

https://dev.ib.pl/ib/otrs/issues/59
https://github.com/OTRS/otrs/pull/1181

to limit filename length to 100 chars and to whitelist allowed characters.

Related: https://dev.ib.pl/ib/otrs/issues/59
Related: https://github.com/OTRS/otrs/pull/1181
Author-Change-Id: IB#1016067

Kernel/System/EmailParser.pm
@@ -689,6 +689,13 @@ sub PartsAttachments {
             String => $Part->head()->recommended_filename(),
             Encode => 'utf-8',
         );
+
+        # cleanup filename
+        $PartData{Filename} = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
+            Filename => $PartData{Filename},
+            Type     => 'Attachment',
+        );
+
         $PartData{ContentDisposition} = $Part->head()->get('Content-Disposition');
         if ( $PartData{ContentDisposition} ) {
             my %Data = $Self->GetContentTypeParams(

Kernel/System/Web/Request.pm
@@ -17,6 +17,7 @@ use File::Path qw();
 
 our @ObjectDependencies = (
     'Kernel::Config',
+    'Kernel::System::Main',
     'Kernel::System::CheckItem',
     'Kernel::System::Encode',
 );
@@ -267,6 +268,12 @@ sub GetUploadAll {
     # replace all devices like c: or d: and dirs for IE!
     $NewFileName =~ s/.:\\(.*)/$1/g;
     $NewFileName =~ s/.*\\(.+?)/$1/g;
+
+    # cleanup filename
+    $NewFileName = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
+        Filename => $NewFileName,
+        Type     => 'Attachment',
+    );
 
     # return a string
     my $Content;

commit c55d644f15cb335fc5968e1b397c8a25354fe473

Empty attachment subject handlign fixed
Fixes empty attachment subject handling.

Fixes: 459ae466100b66d8f264b7d52d832103d2dcce7d
Related: https://dev.ib.pl/ib/otrs/issues/59
Related: https://github.com/OTRS/otrs/pull/1181
Author-Change-Id: IB#1016067

Kernel/System/EmailParser.pm
@@ -725,13 +725,16 @@ sub PartsAttachments {
         my ($SubjectString) = $Part->as_string() =~ m/^Subject: ([^\n]*(\n[ \t][^\n]*)*)/m;
         my $Subject = $Self->_DecodeString( String => $SubjectString );
 
-        # cleanup filename
-        $Subject = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
-            Filename => $Subject,
-            Type     => 'Attachment',
-        );
+        if ( defined($Subject) && ( length($Subject) > 0 ) ) {
 
-        if ( $Subject eq '' ) {
+            # cleanup filename
+            $Subject = $Kernel::OM->Get('Kernel::System::Main')->FilenameCleanUp(
+                Filename => $Subject,
+                Type     => 'Attachment',
+            );
+        }
+
+        if ( !defined($Subject) || ( length($Subject) == 0 ) ) {
             $Self->{NoFilenamePartCounter}++;
             $Subject = "file-$Self->{NoFilenamePartCounter}";
         }