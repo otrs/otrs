otrs/otrs

commit 2a350f0ef7f57c8224c3c73c3d0255f9920793b5

Configurable access to archive searching in agent panel
This mod introduces new SysConfig parameter Ticket::AgentArchiveSystem
that allows admin to disable access for archive searches in agent panel
(does not affect generic agent nor statistics modules). This may help
in protection against overloads in bigger OTRS setups (i.e. fulltext
searches in archive by agents).

Related: https://dev.ib.pl/ib/otrs/issues/113
Author-Change-Id: IB#1073066

Kernel/Config/Files/Ticket.xml
@@ -217,6 +217,17 @@
             </Option>
         </Setting>
     </ConfigItem>
+    <ConfigItem Name="Ticket::AgentArchiveSystem" Required="1" Valid="1">
+        <Description Translatable="1">Activates the ticket archive system search in the agent interface (does not affect generic agent nor statistics modules).</Description>
+        <Group>Ticket</Group>
+        <SubGroup>Core::Ticket</SubGroup>
+        <Setting>
+            <Option SelectedID="1">
+                <Item Key="0">No</Item>
+                <Item Key="1">Yes</Item>
+            </Option>
+        </Setting>
+    </ConfigItem>
     <ConfigItem Name="Ticket::CustomerArchiveSystem" Required="1" Valid="1">
         <Description Translatable="1">Activates the ticket archive system search in the customer interface.</Description>
         <Group>Ticket</Group>

Kernel/Modules/AgentTicketSearch.pm
@@ -1479,7 +1479,8 @@ sub Run {
             },
         );
 
-        if ( $ConfigObject->Get('Ticket::ArchiveSystem') ) {
+        if ( $ConfigObject->Get('Ticket::ArchiveSystem')
+            && $ConfigObject->Get('Ticket::AgentArchiveSystem') ) {
             push @Attributes, (
                 {
                     Key   => 'SearchInArchive',
@@ -1798,7 +1799,8 @@ sub Run {
             Class      => 'Modernize',
         );
 
-        if ( $ConfigObject->Get('Ticket::ArchiveSystem') ) {
+        if ( $ConfigObject->Get('Ticket::ArchiveSystem')
+            && $ConfigObject->Get('Ticket::AgentArchiveSystem') ) {
 
             $Param{SearchInArchiveStrg} = $LayoutObject->BuildSelection(
                 Data => {

Kernel/Output/HTML/LinkObject/Ticket.pm
@@ -755,7 +755,8 @@ sub SearchOptionList {
             };
     }
 
-    if ( $Kernel::OM->Get('Kernel::Config')->Get('Ticket::ArchiveSystem') ) {
+    if ( $Kernel::OM->Get('Kernel::Config')->Get('Ticket::ArchiveSystem')
+        && $Kernel::OM->Get('Kernel::Config')->Get('Ticket::AgentArchiveSystem') ) {
         push @SearchOptionList,
             {
             Key  => 'ArchiveID',

commit a1bcb0fca70a9afbd0ccb1d27880d912deee0b85

Enhanced configurable access to archive searching in agent panel
This mod replaces Ticket::AgentArchiveSystem introduced
in 2a350f0ef7f57c8224c3c73c3d0255f9920793b5 with
Ticket::ArchiveSearchUserGroup which allowes archive searching only
if user does has rw access to group defined
in Ticket::ArchiveSearchUserGroup parameter (no restriction
if this parameter is no defined).

Fixes: 2a350f0ef7f57c8224c3c73c3d0255f9920793b5
Related: https://dev.ib.pl/ib/otrs/issues/113
Author-Change-Id: IB#1073066

Kernel/Config/Files/Ticket.xml
@@ -217,15 +217,12 @@
             </Option>
         </Setting>
     </ConfigItem>
-    <ConfigItem Name="Ticket::AgentArchiveSystem" Required="1" Valid="1">
-        <Description Translatable="1">Activates the ticket archive system search in the agent interface (does not affect generic agent nor statistics modules).</Description>
+    <ConfigItem Name="Ticket::ArchiveSearchUserGroup" Required="0" Valid="0">
+        <Description Translatable="1">Name of group that user must belog to (rw), to be able to search ticket archive (does not affect generic agent nor statistics modules). If not defined - all users are able to search ticket archive.</Description>
         <Group>Ticket</Group>
         <SubGroup>Core::Ticket</SubGroup>
         <Setting>
-            <Option SelectedID="1">
-                <Item Key="0">No</Item>
-                <Item Key="1">Yes</Item>
-            </Option>
+            <String Regex=""></String>
         </Setting>
     </ConfigItem>
     <ConfigItem Name="Ticket::CustomerArchiveSystem" Required="1" Valid="1">

Kernel/Config/Files/XML/Ticket.xml
@@ -162,11 +162,11 @@
             <Item ValueType="Checkbox">1</Item>
         </Value>
     </Setting>
-    <Setting Name="Ticket::AgentArchiveSystem" Required="1" Valid="1">
-        <Description Translatable="1">Activates the ticket archive system search in the agent interface (does not affect generic agent nor statistics modules).</Description>
+    <Setting Name="Ticket::ArchiveSearchUserGroup" Required="0" Valid="0">
+        <Description Translatable="1">Name of group that user must belog to (rw), to be able to search ticket archive (does not affect generic agent nor statistics modules). If not defined - all users are able to search ticket archive.</Description>
         <Navigation>Core::Ticket</Navigation>
         <Value>
-            <Item ValueType="Checkbox">1</Item>
+            <Item ValueType="String" ValueRegex=""></Item>
         </Value>
     </Setting>
     <Setting Name="Ticket::CustomerArchiveSystem" Required="1" Valid="1">

Kernel/Modules/AgentTicketSearch.pm
@@ -1479,14 +1479,44 @@ sub Run {
             },
         );
 
-        if ( $ConfigObject->Get('Ticket::ArchiveSystem')
-            && $ConfigObject->Get('Ticket::AgentArchiveSystem') ) {
-            push @Attributes, (
-                {
-                    Key   => 'SearchInArchive',
-                    Value => Translatable('Archive Search'),
-                },
-            );
+        if ( $ConfigObject->Get('Ticket::ArchiveSystem') ) {
+
+            # allow archive searching if Ticket::ArchiveSearchUserGroup is not defined
+            my $ArchiveSearchAllowed = 1;
+
+            if ( $ConfigObject->Get('Ticket::ArchiveSearchUserGroup') ) {
+
+                # if Ticket::ArchiveSearchUserGroup is defined, allow archive searching
+                # only if user does has rw access to Ticket::ArchiveSearchUserGroup group
+
+                $ArchiveSearchAllowed = 0;
+
+                my $GroupObject = $Kernel::OM->Get('Kernel::System::Group');
+
+                # get current user groups
+                my %UserGroups = $GroupObject->GroupMemberList(
+                    UserID => $Self->{UserID},
+                    Type   => 'rw',
+                    Result => 'HASH',
+                );
+
+                # get group id of Ticket::ArchiveSearchUserGroup
+                my $ArchiveSearchUserGroupID =
+                    $GroupObject->GroupLookup( Group => $ConfigObject->Get('Ticket::ArchiveSearchUserGroup') );
+
+                if ( defined $ArchiveSearchUserGroupID && $UserGroups{$ArchiveSearchUserGroupID} ) {
+                    $ArchiveSearchAllowed = 1;
+                }
+            }
+
+            if ( $ArchiveSearchAllowed ) {
+                push @Attributes, (
+                    {
+                        Key   => 'SearchInArchive',
+                        Value => Translatable('Archive Search'),
+                    },
+                );
+            }
         }
 
         # Dynamic fields
@@ -1799,19 +1829,50 @@ sub Run {
             Class      => 'Modernize',
         );
 
-        if ( $ConfigObject->Get('Ticket::ArchiveSystem')
-            && $ConfigObject->Get('Ticket::AgentArchiveSystem') ) {
 
-            $Param{SearchInArchiveStrg} = $LayoutObject->BuildSelection(
-                Data => {
-                    ArchivedTickets    => Translatable('Archived tickets'),
-                    NotArchivedTickets => Translatable('Unarchived tickets'),
-                    AllTickets         => Translatable('All tickets'),
-                },
-                Name       => 'SearchInArchive',
-                SelectedID => $GetParam{SearchInArchive} || 'NotArchivedTickets',
-                Class      => 'Modernize',
-            );
+        if ( $ConfigObject->Get('Ticket::ArchiveSystem') ) {
+
+            # allow archive searching if Ticket::ArchiveSearchUserGroup is not defined
+            my $ArchiveSearchAllowed = 1;
+
+            if ( $ConfigObject->Get('Ticket::ArchiveSearchUserGroup') ) {
+
+                # if Ticket::ArchiveSearchUserGroup is defined, allow archive searching
+                # only if user does has rw access to Ticket::ArchiveSearchUserGroup group
+
+                $ArchiveSearchAllowed = 0;
+
+                my $GroupObject = $Kernel::OM->Get('Kernel::System::Group');
+
+                # get current user groups
+                my %UserGroups = $GroupObject->GroupMemberList(
+                    UserID => $Self->{UserID},
+                    Type   => 'rw',
+                    Result => 'HASH',
+                );
+
+                # get group id of Ticket::ArchiveSearchUserGroup
+                my $ArchiveSearchUserGroupID =
+                    $GroupObject->GroupLookup( Group => $ConfigObject->Get('Ticket::ArchiveSearchUserGroup') );
+
+                if ( defined $ArchiveSearchUserGroupID && $UserGroups{$ArchiveSearchUserGroupID} ) {
+                    $ArchiveSearchAllowed = 1;
+                }
+            }
+
+            if ( $ArchiveSearchAllowed ) {
+
+                $Param{SearchInArchiveStrg} = $LayoutObject->BuildSelection(
+                    Data => {
+                        ArchivedTickets    => Translatable('Archived tickets'),
+                        NotArchivedTickets => Translatable('Unarchived tickets'),
+                        AllTickets         => Translatable('All tickets'),
+                    },
+                    Name       => 'SearchInArchive',
+                    SelectedID => $GetParam{SearchInArchive} || 'NotArchivedTickets',
+                    Class      => 'Modernize',
+                );
+            }
         }
 
         my %Profiles = $SearchProfileObject->SearchProfileList(

Kernel/Output/HTML/LinkObject/Ticket.pm
@@ -717,6 +717,8 @@ sub SearchOptionList {
 
     my $ParamHook = $Kernel::OM->Get('Kernel::Config')->Get('Ticket::Hook') || 'Ticket#';
 
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
     # search option list
     my @SearchOptionList = (
         {
@@ -746,7 +748,7 @@ sub SearchOptionList {
         },
     );
 
-    if ( $Kernel::OM->Get('Kernel::Config')->Get('Ticket::Type') ) {
+    if ( $ConfigObject->Get('Ticket::Type') ) {
         push @SearchOptionList,
             {
             Key  => 'TypeIDs',
@@ -755,14 +757,45 @@ sub SearchOptionList {
             };
     }
 
-    if ( $Kernel::OM->Get('Kernel::Config')->Get('Ticket::ArchiveSystem')
-        && $Kernel::OM->Get('Kernel::Config')->Get('Ticket::AgentArchiveSystem') ) {
-        push @SearchOptionList,
-            {
-            Key  => 'ArchiveID',
-            Name => Translatable('Archive search'),
-            Type => 'List',
-            };
+
+    if ( $ConfigObject->Get('Ticket::ArchiveSystem') ) {
+
+        # allow archive searching if Ticket::ArchiveSearchUserGroup is not defined
+        my $ArchiveSearchAllowed = 1;
+
+        if ( $ConfigObject->Get('Ticket::ArchiveSearchUserGroup') ) {
+
+            # if Ticket::ArchiveSearchUserGroup is defined, allow archive searching
+            # only if user does has rw access to Ticket::ArchiveSearchUserGroup group
+
+            $ArchiveSearchAllowed = 0;
+
+            my $GroupObject = $Kernel::OM->Get('Kernel::System::Group');
+
+            # get current user groups
+            my %UserGroups = $GroupObject->GroupMemberList(
+                UserID => $Self->{UserID},
+                Type   => 'rw',
+                Result => 'HASH',
+            );
+
+            # get group id of Ticket::ArchiveSearchUserGroup
+            my $ArchiveSearchUserGroupID =
+                $GroupObject->GroupLookup( Group => $ConfigObject->Get('Ticket::ArchiveSearchUserGroup') );
+
+            if ( defined $ArchiveSearchUserGroupID && $UserGroups{$ArchiveSearchUserGroupID} ) {
+                $ArchiveSearchAllowed = 1;
+            }
+        }
+
+        if ( $ArchiveSearchAllowed ) {
+            push @SearchOptionList,
+                {
+                Key  => 'ArchiveID',
+                Name => Translatable('Archive search'),
+                Type => 'List',
+                };
+        }
     }
 
     # add formkey