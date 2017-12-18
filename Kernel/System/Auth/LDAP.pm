0ce119aea5a67bbc9b13c7d64349eb2b76c29d92
Charles Pigott <charlespigott@googlemail.com>
2017-12-18 13:50:15 +0000

    Implement StartTLS support for LDAP auth modules

--- Kernel/System/Auth/LDAP.pm
+++ Kernel/System/Auth/LDAP.pm
@@ -90,6 +90,8 @@ sub new {
         $Self->{Params} = {};
     }
 
+    $Self->{StartTLS} = $ConfigObject->Get( 'AuthModule::LDAP::StartTLS' . $Param{Count} ) || '';
+
     return $Self;
 }
 
@@ -176,6 +178,22 @@ sub Auth {
                 Message  => "Can't connect to $Self->{Host}: $@",
             );
             return;
+        }
+    }
+    if ( $Self->{StartTLS} ) {
+        my $Started = $LDAP->start_tls( verify => $Self->{StartTLS} );
+        if ( !$Started ) {
+            if ( $Self->{Die} ) {
+                die "start_tls on $Self->{Host} failed: $@";
+            }
+            else {
+                $Kernel::OM->Get('Kernel::System::Log')->Log(
+                    Priority => 'error',
+                    Message  => "start_tls: '$Self->{StartTLS}' on $Self->{Host} failed: $@",
+                );
+                $LDAP->disconnect();
+                return;
+            }
         }
     }
     my $Result = '';
