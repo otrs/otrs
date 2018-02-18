223759cbb43614ef80576cfa8da31942dcecba79
andreashergert <andreashergert@10.24.22.99>
2018-02-19 00:08:55 +0100

    Allow array as top-level-XML-Element on REST Response from remote site.

--- Kernel/GenericInterface/Mapping/XSLT.pm
+++ Kernel/GenericInterface/Mapping/XSLT.pm
@@ -108,9 +108,9 @@ sub Map {
     my ( $Self, %Param ) = @_;
 
     # Check data - only accept undef or hash ref.
-    if ( defined $Param{Data} && ref $Param{Data} ne 'HASH' ) {
+    if ( defined $Param{Data} && ref $Param{Data} ne 'HASH' && ref $Param{Data} ne 'ARRAY') {
         return $Self->{DebuggerObject}->Error(
-            Summary => 'Got Data but it is not a hash ref in Mapping XSLT backend!'
+            Summary => 'Got Data but it is not a hash or array ref in Mapping XSLT backend!'
         );
     }
 
