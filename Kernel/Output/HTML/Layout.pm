7d8b30f54207c92669bc648e6419eea22bba48b5
Ogris, Felix J <Felix.Ogris@dts.de>
2018-05-11 11:58:01 +0200

    no non-printable characters in headers

--- Kernel/Output/HTML/Layout.pm
+++ Kernel/Output/HTML/Layout.pm
@@ -2481,8 +2481,9 @@ sub Attachment {
     if ( $Param{Filename} ) {
 
         # IE 10+ supports this
+        my $EncodedFilename = URI::Escape::uri_escape( $Param{Filename} );
         my $URLEncodedFilename = URI::Escape::uri_escape_utf8( $Param{Filename} );
-        $Output .= " filename=\"$Param{Filename}\"; filename*=utf-8''$URLEncodedFilename";
+        $Output .= " filename=\"$EncodedFilename\"; filename*=utf-8''$URLEncodedFilename";
     }
     $Output .= "\n";
 
