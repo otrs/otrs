be26c3ca8b813931355bddad0a2386a2998e0c6b
bernhard <Bernhard.Schmalhofer@gmx.de>
2020-06-08 13:47:22 +0200

    CREATE USER statement for MySQL 8
    Should work on MySQL 5.7 and MariaDB as well.

--- Kernel/Modules/Installer.pm
+++ Kernel/Modules/Installer.pm
@@ -505,8 +505,8 @@ sub Run {
 
                 @Statements = (
                     "CREATE DATABASE `$DB{DBName}` charset utf8",
-                    "GRANT ALL PRIVILEGES ON `$DB{DBName}`.* TO `$DB{OTRSDBUser}`\@`$DB{Host}` IDENTIFIED BY '$DB{OTRSDBPassword}' WITH GRANT OPTION",
-                    "FLUSH PRIVILEGES",
+                    "CREATE USER `$DB{OTOBODBUser}`\@`$Host` IDENTIFIED BY '$DB{OTOBODBPassword}'",
+                    "GRANT ALL PRIVILEGES ON `$DB{DBName}`.* TO `$DB{OTOBODBUser}`\@`$Host` WITH GRANT OPTION",
                 );
             }
 
